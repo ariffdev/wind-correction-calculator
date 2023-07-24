372479771607d55f2670a59e8b266ea11b288175
Abdul Sabit Ariff <58955721+hadid25@users.noreply.github.com>
2023-07-24 10:53:58 +0000

    Done Done Done

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,50 @@
-//to compute the performance for the maximum legal wind, just assume wind is 2m/s
-  function adjust100m(mark, wind){
-    adjusted =  mark - (0.0449 * wind) + (0.009459 * mark * wind) - (0.0042 * Math.pow(wind, 2));
-    return adjusted.toFixed(2)
- }
 
 
-function adjust200m(mark, wind){
-  adjusted =  mark + (0.09 * wind) - (0.01 * Math.pow(wind, 2));
-  return adjusted.toFixed(2);
+const events_coeff_object = {
+  e_100m : {alpha: 0.009459, beta: -0.0449, b : -0.0042},
+  e_200m : {alpha: 0, beta: 0.090, b : -0.010},
+  e_100mH : {alpha: 0, beta: 0.093, b : -0.010},
+  e_110mH : {alpha: 0, beta: 0.093, b : -0.010},
+  e_LJ : {alpha: 0, beta: -0.029, b : 0},
+  e_TJ : {alpha: 0, beta: -0.069, b : 0.009},
 }
 
 
-function adjustSprintHurdles(mark, wind){
-  adjusted = mark + (0.093 * wind) - (0.01 * Math.pow(wind, 2));
-  return adjusted.toFixed(2);
+function correction(event_selected, mark, wind){
+  let alpha = events_coeff_object[event_selected].alpha;
+  let beta = events_coeff_object[event_selected].beta;
+  let b = events_coeff_object[event_selected].b;
+  return ((beta + alpha * mark) * wind) + (b * Math.pow(wind, 2));
 }
 
-function adjustLongJump(mark, wind){
-  adjusted = mark - (0.029 * wind);
-  return adjusted.toFixed(2);
+function compute(event_selected, mark, wind){
+  let zeroAdjustedMark = mark + correction(event_selected, mark, wind);
+  let correctionZerotoTwo = correction(event_selected, mark, wind=2);
+  let maxLegalPerformance = zeroAdjustedMark - correctionZerotoTwo;
+
+  return [zeroAdjustedMark.toFixed(2), maxLegalPerformance.toFixed(2)];
 }
 
-function adjustTripleJump(mark, wind){
-  adjusted =  mark - (0.069 * wind) + (0.009 * Math.pow(wind, 2));
-  return adjusted.toFixed(2);
-}
+
+window.addEventListener("click",()=> {
+  let event_selected = document.getElementById('event-selector').value;
+  let wind = Number(document.getElementById('wind-selector').value);
+  let mark = Number(document.getElementById('mark-selector').value);
+
+  if (event_selected == 'e_TJ' || event_selected == 'e_LJ'){
+    document.getElementById('adjusted-mark-unit-1').innerText = 'm';
+    document.getElementById('adjusted-mark-unit-2').innerText = 'm';
+  }
+
+  document.getElementById('zero-adjusted-mark-output').innerText = compute(event_selected, mark, wind)[0];
+  document.getElementById('max-legal-performance-output').innerText = compute(event_selected, mark, wind)[1];
+})
+
+
+
+
+
+
+
+
 
